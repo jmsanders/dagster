518ab93e69ab2bd393412761292a25eeaa434fc3
gibsondan <daniel@elementl.com>
2020-10-22 17:16:03 -0400

    Temporarily disable hanging windows tests
    Summary: This test is inexplicably hanging on windows since we switched to gRPC, something about trying to load a non-existant module is causing it to hang. Temporarily disable it while we investigate.
    
    Test Plan: BK+Azure
    
    Reviewers: max, alangenfeld, schrockn, prha
    
    Differential Revision: https://dagster.phacility.com/D4874

--- python_modules/dagster/dagster/core/test_utils.py
+++ python_modules/dagster/dagster/core/test_utils.py
@@ -8,6 +8,7 @@ import yaml
 from dagster import (
     DagsterInvariantViolationError,
     Output,
+    Shape,
     SolidDefinition,
     check,
     composite_solid,
@@ -15,10 +16,12 @@ from dagster import (
     seven,
     solid,
 )
+from dagster.core.host_representation import ExternalPipeline
 from dagster.core.instance import DagsterInstance
 from dagster.core.launcher import RunLauncher
 from dagster.core.launcher.default_run_launcher import DefaultRunLauncher
 from dagster.core.launcher.grpc_run_launcher import GrpcRunLauncher
+from dagster.core.storage.pipeline_run import PipelineRun
 from dagster.serdes import ConfigurableClass
 from dagster.utils import merge_dicts
 
@@ -310,3 +313,37 @@ class ExplodingRunLauncher(RunLauncher, ConfigurableClass):
 
     def terminate(self, run_id):
         check.not_implemented("Termination not supported")
+
+
+class MockedRunLauncher(RunLauncher, ConfigurableClass):
+    def __init__(self, inst_data=None):
+        self._inst_data = inst_data
+        self._queue = []
+
+    def launch_run(self, instance, run, external_pipeline):
+        check.inst_param(instance, "instance", DagsterInstance)
+        check.inst_param(run, "run", PipelineRun)
+        check.inst_param(external_pipeline, "external_pipeline", ExternalPipeline)
+        self._queue.append(run)
+        return run
+
+    def queue(self):
+        return self._queue
+
+    @classmethod
+    def config_type(cls):
+        return Shape({})
+
+    @classmethod
+    def from_config_value(cls, inst_data, config_value):
+        return cls(inst_data=inst_data,)
+
+    @property
+    def inst_data(self):
+        return self._inst_data
+
+    def can_terminate(self, run_id):
+        return False
+
+    def terminate(self, run_id):
+        check.not_implemented("Termintation not supported")
